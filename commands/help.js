a423813b964e93ce9c1b92349790565009c09bac
LegendLOL <83415030+LegendLOL@users.noreply.github.com>
2021-08-24 18:48:41 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "help",
+    description: "tells you about the bot, how many commands you can use and how to use it.",
+
+    async run (bot, message, args) {
+
+        const help = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle('Prefix- `lb-`')
+        .setAuthor('Command List', message.author.displayAvatarURL())
+
+        .addFields({
+            name: 'About this bot',
+            value: 'legbot is just i bot i decided to make on my freetime. its still a work in progress and im looking on adding more commands soon.',
+        },
+        {
+            name: 'Information',
+            value: '`some of the commands below are still a work in progress so dont expect it to be organized.`',        
+        },
+        {
+            name: 'all of these commands are visible to everyone and anyone can use them if they want.©',
+            value: '`|info: help, updates, corona, | fun: cat, dog, howgay, joke, meme |`'
+        },)
+
+        message.channel.send(help)
+    }
+}
