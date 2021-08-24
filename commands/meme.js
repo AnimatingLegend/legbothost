cd912f43fc0333b68ef03306230e72c4e2cd1f55
LegendLOL <83415030+LegendLOL@users.noreply.github.com>
2021-08-24 14:59:04 -0400

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -1,7 +1,7 @@
 const got = require('got')
 const { MessageEmbed } = require('discord.js')
 module.exports = {
-    name : 'meme',
+    name : 'memes',
     run : async(client, message) => {
         got('https://www.reddit.com/r/memes/random/.json').then(res => {
             let content = JSON.parse(res.body)
