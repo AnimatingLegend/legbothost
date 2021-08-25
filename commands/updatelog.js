2887c8c3b8ec7572f65593550bbb341a44f9c51d
LegendLOL <83415030+LegendLOL@users.noreply.github.com>
2021-08-24 21:30:37 -0400

    Update updatelog.js

--- commands/updatelog.js
+++ commands/updatelog.js
@@ -10,11 +10,13 @@ module.exports = {
         .setTitle(`Update Log 8/24/21`)
         .setColor(`#3734eb`)
         .addField(`- Made github repository so bot can stay online 24/7 on heroku`)
-        .addField(`- Fixed a Npm error with discord.js (TOOK ME 4 HOURS FHJIDFGDASJKHFGHASJKFDSADHFKAJDSF) thanks color for the help.`)
+        .addField(`- Fixed a Npm error with discord.js (thanks color for the help.)`)
         .addField(`- Added Util code to make my cmds faster (TOTALLY didnt skid the code.)`)
         .addField(`- Added Corona cmd `)
-        .addField(`- added help command. (more cleaner than cmds command)`)
+        .addField(`- added help command. (more cleaner than cmds command) usage: lb-help tells you info about legbot and all the commands.`)
         .addField(`- Removed Cmds command. (if that STILL makes sense.)`)
+        .addField(`- Minor Fixes to meme.js (Got a better api so the memes can be more funnier.)`)
+        .addField(`- Added Bird.js command (birb.) usage: lb-birb sends facts and images of birds.`)
         .setTimestamp();
 
         await message.channel.send(embed)
