ccaa76ce72e564e34a58a1d4d5aa38a5dd6c46a6
LegendLOL <83415030+LegendLOL@users.noreply.github.com>
2021-08-25 15:09:05 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -21,7 +21,7 @@ module.exports = {
         },
         {
             name: 'all of these commands are visible to everyone and anyone can use them if they want.©',
-            value: '`| info: help, updates, corona, | fun: cat, dog, howgay, joke, meme |`'
+            value: '`| info: help, updates, corona, userinfo | fun: cat, dog, birb, howgay, joke, meme, |`'
         },)
 
         message.channel.send(help)
