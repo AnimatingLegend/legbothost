464ce5a70b2f6e63dbb2ed740ed00b54fc91eee9
colorlol <83384149+colorlol@users.noreply.github.com>
2021-08-27 16:35:18 -0400

    Update hangman.js

--- commands/hangman.js
+++ commands/hangman.js
@@ -3,9 +3,9 @@ const { hangman } = require('reconlx')
 module.exports = {
     name : 'hangman',
     run : async(client, message, args) => {
-        if(message.author.id !== '349207063299227648') return;
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('You need manage messages permission.')
         const channel = message.mentions.channels.first() || message.guild.channels.cache.get(args[0])
-        if(!channel) return message.channel.send('Please specify a channel.')
+        if(!channel) return message.channel.send('Please specify a channel')
         const word = args.slice(1).join(" ")
         if(!word) return  message.channel.send('Please specify a word to guess.')
 
