0d520a45a329ff58a69319f405133b155b037dab
LegendLOL <83415030+LegendLOL@users.noreply.github.com>
2021-09-03 19:53:21 -0400

    Update howgay.js

--- commands/howgay.js
+++ commands/howgay.js
@@ -8,9 +8,6 @@ module.exports = {
         let member = message.mentions.users.first() || message.author
 
         let rng = Math.floor(Math.random() * 101);
-        if (member.id == "349207063299227648") rng = "0"
-        if (member.id == "816684213885599754") rng = "100,000,000,000"
-        if (member.id == "421794471525744650") rng = "69 :sunglasses:"
 
         const howgayembed = new Discord.MessageEmbed()
         .setTitle(`Gay Machine Calculator`)
