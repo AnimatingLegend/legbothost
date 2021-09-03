1c694209656098470602ec685436bd3662092a5c
LegendLOL <83415030+LegendLOL@users.noreply.github.com>
2021-09-02 21:19:19 -0400

    Update updatelog.js

--- commands/updatelog.js
+++ commands/updatelog.js
@@ -7,11 +7,12 @@ module.exports = {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
         const embed = new MessageEmbed()
-        .setTitle(`Update Log 8/25/21`)
+        .setTitle(`Update Log 9/2/21 (mini update lawl)`)
+        .addField(`- Removed Hangman.js (because i can)`)
+        .addField(`- Added Weather command (weather is cool) Usage:lb-Weather <country/state>`)
+        .addField(`- Added TicTacToe Command (im god at this game.) lb-tictactoe - game.`)
+        .addField(`- minor updates to howgay.js (whitelisted some peeps lol)`)
         .setColor(`#3734eb`)
-        .addField(`- Minor fixes to: howgay.js - randomized ppls gay percentage`)
-        .addField(`- userinfo.js is not accessable to members due to a lot of bugs with the command.`)
-        .addField(`- Added hangman cmd usage:lb-hangman`)
         .setTimestamp();
 
         await message.channel.send(embed)
