4f6a1bc9efb94926aff6bd8afa91052698a7a71c
LegendLOL <83415030+LegendLOL@users.noreply.github.com>
2021-09-19 19:46:41 -0400

    Update updatelog.js

--- commands/updatelog.js
+++ commands/updatelog.js
@@ -7,9 +7,17 @@ module.exports = {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
         const embed = new MessageEmbed()
-        .setTitle(`Update Log 9/11/21 (stfu about the date)`)
-        .addField(`- Minor Fixes to Userinfo.js - Installed a couple of packages to make the code run. `)
-        .addField(`- Added UserInfo.js back to legbot for more testing purposes (may not work cause im still reworking )`)
+        .setTitle(`Update Log 9/19/21 (LegBot Annoucment, and final updates)`)
+        .addField(`I have made the desicion LegBot will no longer be getting updated due to school, and my own personal projects. Bugs will probably not be fixed due to those decisions in mind, i will put out my LAST few updates to the bot before moving on with school and other projects.`)
+        .addField(`- Updated About me for legbot`)
+        .addField(`- Changed a couple of prompts in 8ball.js`)
+        .addField(`- Removed whitelisted people from howgay.js (sorry color and duck lawl)`)
+        .addField(`- Updated Reddit API for meme.js - memes should be a lot more funnier now`)
+        .addField(`- Changed legbots hosting service to replit (heroku you have to pay for, and its harder to push updates on.)`)
+        .addField(`- Added Squrriel.js back to the repository (there are no facts cause i couldnt get my hands on one :sadge:)| Usage - lb-squrriel `)
+        .addField(`- removed tictactoe.js - impossible bugs i didnt even know how to fix so i decied to discontinue the command and delete it`)
+        .addField(`- removed userinfo.js - though this would of been a cool command to have, the bugs were too much and i couldnt figure out what was the problem so i just discontinuted it`)
+        .addField(`Those are gonna be the LAST updates for this bot. i appreciate you guys sticking by me when working on this bot, but all good times must come to an end.. thank you guys for using/testing legbot! <3 ~ legend.#7654`)
         .setColor("RANDOM")
         .setTimestamp();
 
