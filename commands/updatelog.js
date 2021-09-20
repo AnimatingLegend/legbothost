b816dee0bb0026c4572628f51aeba8500f1ec3af
legendlol <83415030+LegendLOL@users.noreply.github.com>
2021-09-19 20:39:35 -0400

    Update updatelog.js

--- commands/updatelog.js
+++ commands/updatelog.js
@@ -17,7 +17,6 @@ module.exports = {
         .addField(`- Added Squrriel.js back to the repository (there are no facts cause i couldnt get my hands on one :sadge:)| Usage - lb-squrriel `)
         .addField(`- removed tictactoe.js - impossible bugs i didnt even know how to fix so i decied to discontinue the command and delete it`)
         .addField(`- removed userinfo.js - though this would of been a cool command to have, the bugs were too much and i couldnt figure out what was the problem so i just discontinuted it`)
-        .addField(`Those are gonna be the LAST updates for this bot. i appreciate you guys sticking by me when working on this bot, but all good times must come to an end.. thank you guys for using/testing legbot! <3 ~ legend.#7654`)
         .setColor("RANDOM")
         .setTimestamp();
 
