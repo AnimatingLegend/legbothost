2f7d6f162d45ee540a6261728f58746a5b90a265
legendlol <83415030+LegendLOL@users.noreply.github.com>
2021-09-19 21:01:50 -0400

    Update updatelog.js

--- commands/updatelog.js
+++ commands/updatelog.js
@@ -14,7 +14,7 @@ module.exports = {
         .addField(`- Removed whitelisted people from howgay.js (sorry color and duck lawl)`)
         .addField(`- Updated Reddit API for meme.js - memes should be a lot more funnier now`)
         .addField(`- Changed legbots hosting service to replit (heroku you have to pay for, and its harder to push updates on.)`)
-        .addField(`- Added Squrriel.js back to the repository (there are no facts cause i couldnt get my hands on one :sadge:)| Usage - lb-squrriel `)
+        .addField(`- removed Squrriel.js  - impossible bugs i didnt even know how to fix so i decied to discontinue the command and delete it `)
         .addField(`- removed tictactoe.js - impossible bugs i didnt even know how to fix so i decied to discontinue the command and delete it`)
         .addField(`- removed userinfo.js - though this would of been a cool command to have, the bugs were too much and i couldnt figure out what was the problem so i just discontinuted it`)
         .setColor("RANDOM")
