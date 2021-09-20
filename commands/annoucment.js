32e3a7bb34cb940b4a0b0a9af5508fb4e606a402
legendlol <83415030+LegendLOL@users.noreply.github.com>
2021-09-19 22:57:09 -0400

    Update annoucment.js

--- commands/annoucment.js
+++ commands/annoucment.js
@@ -8,7 +8,9 @@ module.exports = {
 
         const embed = new MessageEmbed()
         .setTitle(`LegBot Annoucment`)
-        .addField('I have came to the decision that i will no longer be working/updating legbot anymore. The reasons too this is because of school, and my own personal projects. Though the bot will stay online 24/7, there will be no more updates starting 10/1/21. **Thank you for using LegBot <3 ~ legend.#7654**')
+        .addField('I have came to the decision that i will no longer be working/updating legbot anymore. The reasons too this is because of school, and my own personal projects. Though the bot will stay online 24/7, there will be no more updates starting 10/1/21.')
+        .addField('I would like to thank you guys for using/testing LegBot while it was in development, I will also like to thank color.#8482 for helping me out with some commands aswell. i appreciate the love and support, and **thank you for testing Legbot!**')
+        .addField(`* ~ legend.#7654 * `)
         .setTimestamp();
 
         await message.channel.send(embed)
