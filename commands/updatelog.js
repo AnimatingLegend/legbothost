2e1d877d1f2ac27619fc81512fc99c296f03794b
legendlol <83415030+LegendLOL@users.noreply.github.com>
2021-09-19 22:45:44 -0400

    Update updatelog.js

--- commands/updatelog.js
+++ commands/updatelog.js
@@ -7,8 +7,7 @@ module.exports = {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
 
         const embed = new MessageEmbed()
-        .setTitle(`Update Log 9/19/21 (final updates)`)
-        .addField(`I have made the desicion LegBot will no longer be getting updated due to school, and my own personal projects. thank you guys for testing this bot out! ~ legend.#7654`)
+        .setTitle(`Update Log 9/19/21`)
         .addField(`- Updated About me for legbot`)
         .addField(`- Changed a couple of prompts in 8ball.js`)
         .addField(`- Removed whitelisted people from howgay.js (sorry color and duck lawl)`)
@@ -17,6 +16,7 @@ module.exports = {
         .addField(`- removed Squrriel.js  - impossible bugs i didnt even know how to fix so i decied to discontinue the command and delete it `)
         .addField(`- removed tictactoe.js - impossible bugs i didnt even know how to fix so i decied to discontinue the command and delete it`)
         .addField(`- removed userinfo.js - though this would of been a cool command to have, the bugs were too much and i couldnt figure out what was the problem so i just discontinuted it`)
+        .addField(`- added annoucment.js | important annoucment to make regarding legbot (lb-legbot)`)
         .setColor("RANDOM")
         .setTimestamp();
 
